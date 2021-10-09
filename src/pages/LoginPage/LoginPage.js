59554210690719994ed0bb73c180a384e40c54be
caolinh22 <48510866+caolinh22@users.noreply.github.com>
2021-10-09 16:25:03 +0800

    update login page

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,106 +1,65 @@
-/* eslint-disable jsx-a11y/alt-text */
-import React, { useState } from 'react';
-import axios from 'axios';
-// import { useProfileHandler } from '../../states/profile';
-import { Link } from 'react-router-dom';
-import validator from 'validator';
-import './style.scss';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as Yup from 'yup';
+
 const LoginPage = () => {
-  const [message, setMessage] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [disableButton, setDisableButton] = useState(true);
-  //demo purpose only
-  // const { onLogin } = useProfileHandler();
-
-  const validateInput = (email, password) => {
-    if (email === '' || password === '') {
-      setMessage('Please insert email and password');
-    } else if (!validator.isEmail(email)) {
-      console.log(1);
-      setMessage('Please enter valid email');
-    } else {
-      setDisableButton(false);
-    }
-  };
-
-  const onChangeEmail = (e) => {
-    setDisableButton(false);
-    setEmail(e.target.value);
-  };
-  const onChangePassword = (e) => {
-    setDisableButton(false);
-    setPassword(e.target.value);
-  };
-  const submit = (e) => {
-    e.preventDefault();
-    validateInput(email, password);
-    axios
-      .post(
-        '/auth/login',
-        {
-          email,
-          password,
-        },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        },
-      )
-      .then(({ data }) => {
-        if (data.success) {
-          window.location.href = '/dashboard';
-        } else {
-          setMessage(data.message);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().required('Email is required').email('Email is invalid'),
+    password: Yup.string()
+      .required('Password is required')
+      .min(6, 'Password must be at least 6 characters')
+      .max(40, 'Password must not exceed 40 characters'),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+  });
+  const onSubmit = (data) => {
+    console.log(JSON.stringify(data, null, 2));
   };
 
   return (
-    <>
-      <div className='wrapper'>
-        <div className='container'>
-          <div className='title'>
-            <h1>Login</h1>
-            <p>Enter your details below to continue</p>
-          </div>
-          <div className='card'>
-            <form method='POST' action='/'>
-              <input
-                id='email'
-                type='text'
-                name='email'
-                required
-                autoComplete='email'
-                autoFocus
-                placeholder='Email'
-                onChange={onChangeEmail}
-              />
-              <input
-                id='password'
-                type='password'
-                name='password'
-                placeholder='Password'
-                required
-                onChange={onChangePassword}
-              />
-              <p className='invalid-feedback' role='alert'>
-                <strong>{message}</strong>
-              </p>
-              <button type='submit' onClick={submit} disabled={disableButton}>
-                LOGIN
-              </button>
-            </form>
-            <div className='signup'>Don't have an account?</div>
-            <Link to='/register'>Sign up here</Link>
-          </div>
+    <div className='register-form'>
+      <div className='container'>
+        <div className='title'>
+          <h1>Login</h1>
+          <p>Enter your details below to continue</p>
         </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className='form-group'>
+            <label>Email</label>
+            <input
+              name='email'
+              type='text'
+              {...register('email')}
+              className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+            />
+            <div className='invalid-feedback'>{errors.email?.message}</div>
+          </div>
+
+          <div className='form-group'>
+            <label>Password</label>
+            <input
+              name='password'
+              type='password'
+              {...register('password')}
+              className={`form-control ${errors.password ? 'is-invalid' : ''}`}
+            />
+            <div className='invalid-feedback'>{errors.password?.message}</div>
+          </div>
+          <div className='button-group'>
+            <button type='submit' className='btn btn-primary' style={{width: '100%' }}>
+              Login
+            </button>
+          </div>
+        </form>
       </div>
-    </>
+    </div>
   );
 };
 
