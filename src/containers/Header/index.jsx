f3d0c173d4a100b439da406096a6cf9abe8a5250
huyldth1908041 <huyldth1908041>
2021-10-13 10:29:18 +0700

    fix header

--- src/containers/Header/index.jsx
+++ src/containers/Header/index.jsx
@@ -1,16 +1,18 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { AiOutlineHome, AiOutlineHeart, AiOutlineCompass } from 'react-icons/ai';
+import { AiOutlineHome, AiOutlineHeart, AiOutlineCompass, AiOutlineLogin, AiOutlineCloudUpload } from 'react-icons/ai';
 import { BsPersonCircle } from 'react-icons/bs';
 import './style.scss';
 import { privateRoute } from '../../routes';
 import { Link } from 'react-router-dom';
 import { useAuthentication } from '../../hooks';
+import { BiExit } from 'react-icons/all';
+import { Button } from 'antd';
 
 function Header() {
   const { user, logout } = useAuthentication();
   const [openProfile, setOpenProfile] = useState(false);
   const ref = useRef();
-  
+
   const toggleProfile = () => {
     setOpenProfile(!openProfile);
   };
@@ -29,7 +31,9 @@ function Header() {
   }, [openProfile]);
   return (
     <div className='header-content'>
-      <div className='logo'>MEME PORTAL</div>
+      <div className='logo'>
+        <Link to={privateRoute.home.path}>MEME PORTAL</Link>
+      </div>
       <div className='search'>
         <input type='text' placeholder='Search' />
       </div>
@@ -43,23 +47,36 @@ function Header() {
         <Link to={privateRoute.home.path}>
           <AiOutlineCompass />
         </Link>
-        {user && (
-          <>
-            <button onClick={toggleProfile} ref={ref}>
-              <div className='account'>
-                <img src={user.avatar || '/images/default-avatar.jpg'} height='100%' width='100%' alt='profile' />
-              </div>
-              <div className='name'>{user.fullName}</div>
-            </button>
-            {openProfile && (
-              <div className='dropdown-content'>
-                <Link to={privateRoute.home.path}>
-                  <BsPersonCircle /> Profile
-                </Link>
-                <button onClick={logout}>Logout</button>
-              </div>
-            )}
-          </>
+        {user ? (
+          <div className='d-flex justify-content-center align-items-center'>
+            <div ref={ref} className="header-content-container">
+              <button onClick={toggleProfile}>
+                <div className='account'>
+                  <img src={user.avatar || '/images/default-avatar.jpg'} height='100%' width='100%' alt='profile' />
+                </div>
+                <div className='name'>{user.fullName}</div>
+              </button>
+              {openProfile && (
+                <div className='dropdown-content'>
+                  <Link to={privateRoute.home.path}>
+                    <BsPersonCircle /> Profile
+                  </Link>
+                  <button onClick={() => logout()}>
+                    <BiExit className='button-icon' />Logout
+                  </button>
+                </div>
+              )}
+            </div>
+            <Link className='header-btn' to={privateRoute.create.path}>
+              <AiOutlineCloudUpload className='button-icon' /> Create
+            </Link>
+          </div>
+        ) : (
+          <div>
+            <Link className='header-btn' to='/login'>
+              <AiOutlineLogin className='button-icon' /> Login
+            </Link>
+          </div>
         )}
       </div>
     </div>
