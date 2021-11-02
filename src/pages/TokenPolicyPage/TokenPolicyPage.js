cb8ea0d8c299c73cbbe5c2f561bd2c8d44458636
caolinh22 <48510866+caolinh22@users.noreply.github.com>
2021-11-02 23:11:37 +0800

    create route token policy

---
+++ src/pages/TokenPolicyPage/TokenPolicyPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.scss';
+const RegisterPage = () => {
+  return <div className='register-form'>Plicy</div>;
+};
+
+export default RegisterPage;
