8aaf8c259b40369416527663aaaa6893c8e2c5e9
huyldth1908041 <huyldth1908041>
2022-06-12 08:21:41 +0700

    fix conflict in app.js

--- src/App.js
+++ src/App.js
@@ -2,29 +2,24 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { PrivateLayout } from './layouts';
 import { LoginPage } from './pages/LoginPage';
 import { RegisterPage } from './pages/RegisterPage';
-import { Provider } from 'react-redux';
-import store from './states';
 import { Toaster } from 'react-hot-toast';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import ScrollToTopButton from './components/ScrollToTopButton/ScrollToTopButton';
 import { initFacebookSdk } from './utils';
-<<<<<<< HEAD
 import updater from './updater';
-=======
 import ReactGA from 'react-ga';
 import { useEffect } from 'react';
->>>>>>> 8d5c9a9a040d4f6d4d86d7005320ec4559e9e08e
+
 
 const queryClient = new QueryClient();
 initFacebookSdk().then(App);
 
-<<<<<<< HEAD
-=======
+
 
 const TRACKING_ID = 'UA-199508261-1'; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
 
->>>>>>> 8d5c9a9a040d4f6d4d86d7005320ec4559e9e08e
+
 function App() {
   useEffect(() => {
     ReactGA.pageview(window.location.pathname + window.location.search);
