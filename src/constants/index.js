6e5df038a92a457e56eb5fa5ea8034ab994df531
lilhuy0405 <luuhuy040501.travail@gmail.com>
2023-01-17 16:07:21 +0700

    update api url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
-export const API_URL = process.env.REACT_APP_API_URL || 'https://memeportalapi.xyz/api/v1';
+export const API_URL = process.env.REACT_APP_API_URL || 'http://haicodeapi.click/api/v1';
 export const FACEBOOK_APP_ID = process.env.REACT_APP_FACEBOOK_APP_ID || '586707649427850';
 export const PROFILE_STORAGE_KEY = 'haicode-profile';
